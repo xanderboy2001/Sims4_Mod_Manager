2ac1671ff4b3555ed9642d1fb0e62ae5f3680c21
Alex Christian <alexanderechristian@gmail.com>
2025-07-21 20:02:50 -0400

    Script to perform first-launch tasks

---
+++ sims4_mod_manager/setup.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+from sims4_mod_manager.utils import config_dir, get_mods_dir
+
+def is_first_run() -> bool:
+    return not config_dir.exists()
+
+
+def create_default_config():
+    config_dir.parent.mkdir(parents=True, exist_ok=True)
+    default_config = {
+        "mods_folder": str(get_mods_dir())
+    }
