31e94b4aa5ab9ddc61dd5bd562abb5eb07e734ed
Alex Christian <alexanderechristian@gmail.com>
2025-07-21 21:22:10 -0400

    Split arg parser to its own function

--- sims4_mod_manager/cli.py
+++ sims4_mod_manager/cli.py
@@ -1,38 +1,49 @@
 import argparse
 import sys
 
+from sims4_mod_manager.config import print_config
 from sims4_mod_manager.mods.manager import print_directory_tree
 from sims4_mod_manager.setup import first_run
 from sims4_mod_manager.utils import get_mods_dir
 
+arg_parser = argparse.ArgumentParser(
+    prog="sims4-mod-manager",
+    description="A simple mod manager for The Sims 4",
+)
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="sims4-mod-manager",
-        description="A simple mod manager for The Sims 4",
-    )
 
-    parser.add_argument(
-        "--first_run",
+def parse_args():
+
+    subparsers = arg_parser.add_subparsers(dest="command", required=True)
+
+    subparsers.add_parser("list", help="List all installed mods")
+    subparsers.add_parser(
+        "init",
         help="""Simulates running the program for the first time
         (WARNING: THIS WILL REMOVE ALL SETTINGS AND RESET THE
         PROGRAM TO ITS FACTORY DEFAULTS)""",
-        action="store_true",
     )
+    parser_dump_config = subparsers.add_parser(
+        "dump_config", help="Print the current configuration.")
 
-    subparsers = parser.add_subparsers(dest="command", required=True)
+    parser_dump_config.add_argument("--pretty",
+                                    action="store_true",
+                                    help="Pretty-print the config")
 
-    subparsers.add_parser("list", help="List all installed mods")
+    return arg_parser.parse_args()
 
-    args = parser.parse_args()
 
-    if args.first_run:
-        print("Simulating first run...")
-        first_run()
-
+def main():
+    args = parse_args()
     if args.command == "list":
         print("Listing installed mods...")
         print_directory_tree(get_mods_dir())
+    elif args.command == "init":
+        print("Simulating first run...")
+        first_run()
+    elif args.command == "dump_config":
+        print("Dumping config...")
+        print_config(args.pretty)
     else:
-        parser.print_help()
+        arg_parser.print_help()
         sys.exit(1)
