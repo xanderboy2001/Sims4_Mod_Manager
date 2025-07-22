b909ac02f3bc40a722b301d2df654c5452c38ec5
Alex Christian <alexanderechristian@gmail.com>
2025-07-21 20:39:59 -0400

    Added conditional check to run first_run function on first run

--- sims4_mod_manager/__main__.py
+++ sims4_mod_manager/__main__.py
@@ -1,4 +1,8 @@
 from sims4_mod_manager.cli import main
+from sims4_mod_manager.setup import create_default_config, is_first_run
 
 if __name__ == "__main__":
+    if is_first_run():
+        print("This is the first run. Creating default config...")
+        create_default_config()
     main()
