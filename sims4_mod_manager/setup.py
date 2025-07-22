5d4224543fdc478de0721862c643e84681e17c34
Alex Christian <alexanderechristian@gmail.com>
2025-07-21 20:17:01 -0400

    finished create_default_config and wrote load_config functions

--- sims4_mod_manager/setup.py
+++ sims4_mod_manager/setup.py
@@ -1,3 +1,5 @@
+import json
+
 from sims4_mod_manager.utils import config_path, get_mods_dir
 
 
@@ -8,3 +10,10 @@ def is_first_run() -> bool:
 def create_default_config():
     config_path.parent.mkdir(parents=True, exist_ok=True)
     default_config = {"mods_folder": str(get_mods_dir())}
+    with config_path.open("w", encoding="utf-8") as f:
+        json.dump(default_config, f, indent=4)
+
+
+def load_config():
+    with config_path.open("r", encoding="utf-8") as f:
+        return json.load(f)
