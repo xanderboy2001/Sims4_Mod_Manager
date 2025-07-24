179872af6d0c6a069ffacee02d9f1f25a983bb0a
Alex Christian <alexanderechristian@gmail.com>
2025-07-24 16:45:36 -0400

    Better handling of default values

--- sims4_mod_manager/mods/metadata.py
+++ sims4_mod_manager/mods/metadata.py
@@ -8,9 +8,12 @@ EXTENSION_MAP = {".package": "Package", ".ts4script": "Script"}
 DATA_FILE = data_dir / "metadata.json"
 
 
-def get_metadata(filetypes: str | list[str]) -> list[dict[str, str]]:
-    if isinstance(filetypes, str):
-        filetypes = [filetypes]
+def get_metadata(filetypes: str | list[str] = None) -> list[dict[str, str]]:
+    if not filetypes:
+        filetypes = ["package", "script"]
+    else:
+        if isinstance(filetypes, str):
+            filetypes = [filetypes]
     directory = get_mods_dir()
     metadata_list = []
     patterns = []
@@ -33,6 +36,8 @@ def get_metadata(filetypes: str | list[str]) -> list[dict[str, str]]:
 
 
 def write_metadata(metadata: list[dict[str, str]]) -> None:
+    if not metadata:
+        metadata = get_metadata()
     data_dir.mkdir(parents=True, exist_ok=True)
     with DATA_FILE.open("w", encoding="utf-8") as f:
         json.dump(metadata, f, indent=4)
